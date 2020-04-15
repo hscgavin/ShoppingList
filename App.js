7633d1b21cc29794b4dbb420b8320b4ed0d40656
Gavin He <hscgavin@gmail.com>
2020-04-15 15:57:06 +1000

    Move to stylesheet

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
